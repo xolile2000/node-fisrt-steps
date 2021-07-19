eaa23891d8d73155a511d8668272fdee63f46c3c
xolile2000 <xolilentanzi43@gmail.com>
2021-07-19 16:40:23 +0200

    update

--- greet_xola.js
+++ greet_xola.js
@@ -1,4 +1,7 @@
 const chalk = require('chalk');
+const figlet = require("figlet");
 const greet = require('./greet');
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
+const styledMessage = chalk.bgGreen.black(figlet.textSync (greet('Xola')));
 console.log(styledMessage)
+
+
